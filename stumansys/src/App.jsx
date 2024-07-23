b3e3c55cc3affdc0965f647fde3a63247d9d35fb
mythilisaminathan <mythilisaminathan2501@gmai.com>
2024-07-23 15:41:32 +0530

    first commit

---
+++ stumansys/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import StudentsData from './Components/StudentsData'
+
+const App = () => {
+  return (
+    <div>
+    <StudentsData/>
+    </div>
+  )
+}
+
+export default App
